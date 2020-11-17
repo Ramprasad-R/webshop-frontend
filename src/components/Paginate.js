2371a65131d264d5961228e8d83a65d7b5a4da9a
Ramprasad-R <ramprasad.info@gmail.com>
2020-11-17 16:00:55 +0100

    updated pagenate

--- src/components/Paginate.js
+++ src/components/Paginate.js
@@ -6,6 +6,19 @@ const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
   return (
     pages > 1 && (
       <Pagination>
+        {page > 1 && (
+          <LinkContainer
+            to={
+              !isAdmin
+                ? keyword
+                  ? `/search/${keyword}/page/${page - 1}`
+                  : `/page/${page - 1}`
+                : `/admin/productlist/${page - 1}`
+            }
+          >
+            <Pagination.Item>«</Pagination.Item>
+          </LinkContainer>
+        )}
         {[...Array(pages).keys()].map((x) => (
           <LinkContainer
             key={x + 1}
@@ -20,6 +33,19 @@ const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
             <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
           </LinkContainer>
         ))}
+        {page < pages && (
+          <LinkContainer
+            to={
+              !isAdmin
+                ? keyword
+                  ? `/search/${keyword}/page/${page + 1}`
+                  : `/page/${page + 1}`
+                : `/admin/productlist/${page + 1}`
+            }
+          >
+            <Pagination.Item>»</Pagination.Item>
+          </LinkContainer>
+        )}
       </Pagination>
     )
   )
--- src/screens/ProductListScreen.js
+++ src/screens/ProductListScreen.js
@@ -48,7 +48,7 @@ const ProductListScreen = ({ history, match }) => {
     if (successCreate) {
       history.push(`/admin/product/${createdProduct._id}/edit`)
     } else {
-      dispatch(listProducts('', pageNumber))
+      dispatch(listProducts('', '', '', pageNumber))
     }
   }, [
     dispatch,
